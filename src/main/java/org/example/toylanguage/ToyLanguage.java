c4dca9dbb0e85b55afc55a1ab7dc2a6888f097c6
Alexander Makeev <alexmakeev1995@gmail.com>
2022-02-20 16:24:12 +0300

    Create ToyLanguageTest

---
+++ src/main/java/org/example/toylanguage/ToyLanguage.java
@@ -0,0 +1,8 @@
+package org.example.toylanguage;
+
+import java.nio.file.Path;
+
+public class ToyLanguage {
+    public void execute(Path path) {
+    }
+}
---
+++ src/test/java/org/example/toylanguage/ToyLanguageTest.java
@@ -0,0 +1,42 @@
+package org.example.toylanguage;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ToyLanguageTest {
+
+    @Test
+    void execute() throws URISyntaxException, IOException {
+        URL resource = getClass().getClassLoader().getResource("test.toy");
+        Path path = Paths.get(resource.toURI());
+
+        try (InputStream in = new ByteArrayInputStream("Alexander\n5\nyes".getBytes());
+             ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             PrintStream out = new PrintStream(baos)) {
+
+            System.setIn(in);
+            System.setOut(out);
+
+            ToyLanguage lang = new ToyLanguage();
+            lang.execute(path);
+
+            assertEquals(
+                    "enter \"your name\" >>> " +
+                            "enter \"your experience in years\" >>> " +
+                            "enter \"do you like programming\" >>> " +
+                            "Person [ name = Alexander, experience = 5, is_developer = true ]\n" +
+                            "hey Alexander!\n" +
+                            "you had started your career in 2017\n",
+                    baos.toString()
+            );
+        }
+    }
+
+}
