34aaf5e48f6b2a5ac8d73356896be0ba77e702a9
Alexander Makeev <alexmakeev1995@gmail.com>
2022-02-20 16:26:10 +0300

    Complete ToyLanguage

--- src/main/java/org/example/toylanguage/ToyLanguage.java
+++ src/main/java/org/example/toylanguage/ToyLanguage.java
@@ -1,8 +1,23 @@
 package org.example.toylanguage;
 
+import lombok.SneakyThrows;
+import org.example.toylanguage.statement.Statement;
+import org.example.toylanguage.token.Token;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ToyLanguage {
+
+    @SneakyThrows
     public void execute(Path path) {
+        String source = Files.readString(path);
+        LexicalParser lexicalParser = new LexicalParser(source);
+        List<Token> tokens = lexicalParser.parse();
+        StatementParser statementParser = new StatementParser(tokens);
+        Statement statement = statementParser.parse();
+        statement.execute();
     }
+
 }
