8cfc2f04c6557a3fd6b3aa3d28fe623b03fe796d
Alexander Makeev <alexmakeev1995@gmail.com>
2022-03-02 23:08:00 +0300

    Implementing Dijkstra's two-stack algorithm

--- src/main/java/org/example/toylanguage/StatementParser.java
+++ src/main/java/org/example/toylanguage/StatementParser.java
@@ -7,10 +7,7 @@ import org.example.toylanguage.exception.SyntaxException;
 import org.example.toylanguage.expression.Expression;
 import org.example.toylanguage.expression.StructureExpression;
 import org.example.toylanguage.expression.VariableExpression;
-import org.example.toylanguage.expression.operator.BinaryOperatorExpression;
-import org.example.toylanguage.expression.operator.Operator;
-import org.example.toylanguage.expression.operator.OperatorExpression;
-import org.example.toylanguage.expression.operator.UnaryOperatorExpression;
+import org.example.toylanguage.expression.operator.*;
 import org.example.toylanguage.expression.value.LogicalValue;
 import org.example.toylanguage.expression.value.NumericValue;
 import org.example.toylanguage.expression.value.TextValue;
@@ -46,29 +43,29 @@ public class StatementParser {
     }
 
     private Statement parseExpression() {
-        Token token = next(TokenType.Keyword, TokenType.Variable);
+        Token token = next(TokenType.Keyword, TokenType.Variable, TokenType.Operator);
         switch (token.getType()) {
             case Variable:
-                next(TokenType.Operator, "="); //skip equals
+            case Operator:
+                position--;
+                Expression value = new ExpressionReader().readExpression();
 
-                Expression value;
-                if (peek(TokenType.Keyword, "new")) {
-                    value = readInstance();
+                if (value instanceof AssignmentOperator) {
+                    VariableExpression variable = (VariableExpression) ((AssignmentOperator) value).getLeft();
+                    return new AssignStatement(variable.getName(), ((AssignmentOperator) value).getRight(), variables::put);
                 } else {
-                    value = readExpression();
+                    throw new SyntaxException(String.format("Unsupported statement: `%s`", value));
                 }
-
-                return new AssignStatement(token.getValue(), value, variables::put);
             case Keyword:
                 switch (token.getValue()) {
                     case "print":
-                        Expression expression = readExpression();
+                        Expression expression = new ExpressionReader().readExpression();
                         return new PrintStatement(expression);
                     case "input":
                         Token variable = next(TokenType.Variable);
                         return new InputStatement(variable.getValue(), scanner::nextLine, variables::put);
                     case "if":
-                        Expression condition = readExpression();
+                        Expression condition = new ExpressionReader().readExpression();
                         next(TokenType.Keyword, "then"); //skip then
 
                         ConditionStatement conditionStatement = new ConditionStatement(condition);
@@ -100,74 +97,8 @@ public class StatementParser {
         }
     }
 
-    private Expression readInstance() {
-        next(TokenType.Keyword, "new"); //skip new
-
-        Token type = next(TokenType.Variable);
-
-        List<Expression> arguments = new ArrayList<>();
-
-        if (peek(TokenType.GroupDivider, "[")) {
-
-            next(TokenType.GroupDivider, "["); //skip open square bracket
-
-            while (!peek(TokenType.GroupDivider, "]")) {
-                Expression value = readExpression();
-                arguments.add(value);
-            }
-
-            next(TokenType.GroupDivider, "]"); //skip close square bracket
-        }
-
-        StructureDefinition definition = structures.get(type.getValue());
-        if (definition == null) {
-            throw new SyntaxException(String.format("Structure is not defined: %s", type.getValue()));
-        }
-        return new StructureExpression(definition, arguments, variables::get);
-    }
-
-
-    @SneakyThrows
-    private Expression readExpression() {
-        Expression left = nextExpression();
-
-        //recursively read an expression
-        while (peek(TokenType.Operator)) {
-            Token operation = next(TokenType.Operator);
-            Operator operator = Operator.getType(operation.getValue());
-            Class<? extends OperatorExpression> operatorType = operator.getType();
-            if (BinaryOperatorExpression.class.isAssignableFrom(operatorType)) {
-                Expression right = nextExpression();
-                left = operatorType
-                        .getConstructor(Expression.class, Expression.class)
-                        .newInstance(left, right);
-            } else if (UnaryOperatorExpression.class.isAssignableFrom(operatorType)) {
-                left = operatorType
-                        .getConstructor(Expression.class)
-                        .newInstance(left);
-            }
-        }
-
-        return left;
-    }
-
-    private Expression nextExpression() {
-        Token token = next(TokenType.Variable, TokenType.Numeric, TokenType.Logical, TokenType.Text);
-        String value = token.getValue();
-        switch (token.getType()) {
-            case Numeric:
-                return new NumericValue(Integer.parseInt(value));
-            case Logical:
-                return new LogicalValue(Boolean.valueOf(value));
-            case Text:
-                return new TextValue(value);
-            case Variable:
-            default:
-                return new VariableExpression(value, variables::get, variables::put);
-        }
-    }
-
     private Token next(TokenType type, TokenType... types) {
+        skipLineBreaks();
         TokenType[] tokenTypes = ArrayUtils.add(types, type);
         if (position < tokens.size()) {
             Token token = tokens.get(position);
@@ -181,6 +112,7 @@ public class StatementParser {
     }
 
     private Token next(TokenType type, String value) {
+        skipLineBreaks();
         if (position < tokens.size()) {
             Token token = tokens.get(position);
             if (token.getType() == type && token.getValue().equals(value)) {
@@ -192,7 +124,13 @@ public class StatementParser {
         throw new SyntaxException(String.format("After `%s` declaration expected `%s, %s` lexeme", previousToken, type, value));
     }
 
+    private Token next() {
+        skipLineBreaks();
+        return tokens.get(position++);
+    }
+
     private boolean peek(TokenType type, String content) {
+        skipLineBreaks();
         if (position < tokens.size()) {
             Token token = tokens.get(position);
             return type == token.getType() && token.getValue().equals(content);
@@ -200,11 +138,127 @@ public class StatementParser {
         return false;
     }
 
-    private boolean peek(TokenType type) {
-        if (position < tokens.size()) {
-            Token token = tokens.get(position);
-            return token.getType() == type;
+    private void skipLineBreaks() {
+        while (tokens.get(position).getType() == TokenType.LineBreak && ++position != tokens.size()) ;
+    }
+
+    private class ExpressionReader {
+        private final Stack<Expression> operands;
+        private final Stack<Operator> operators;
+
+        private ExpressionReader() {
+            this.operands = new Stack<>();
+            this.operators = new Stack<>();
         }
-        return false;
+
+        private Expression readExpression() {
+            while (peek(TokenType.Operator, TokenType.Variable, TokenType.Numeric, TokenType.Logical, TokenType.Text)) {
+                Token token = next();
+                switch (token.getType()) {
+                    case Operator:
+                        Operator operator = Operator.getType(token.getValue());
+                        switch (operator) {
+                            case LeftParen:
+                                operators.push(operator);
+                                break;
+                            case RightParen:
+                                //until left bracket is not reached
+                                while (!operators.empty() && operators.peek() != Operator.LeftParen)
+                                    applyTopOperator();
+                                operators.pop(); //pop left bracket
+                                break;
+                            default:
+                                //until top operator has greater precedence
+                                while (!operators.isEmpty() && operators.peek().greaterThan(operator))
+                                    applyTopOperator();
+                                operators.push(operator); // finally, add less prioritized operator
+                        }
+                        break;
+                    default:
+                        String value = token.getValue();
+                        Expression operand;
+                        switch (token.getType()) {
+                            case Numeric:
+                                operand = new NumericValue(Integer.parseInt(value));
+                                break;
+                            case Logical:
+                                operand = new LogicalValue(Boolean.valueOf(value));
+                                break;
+                            case Text:
+                                operand = new TextValue(value);
+                                break;
+                            case Variable:
+                            default:
+                                if (!operators.isEmpty() && operators.peek() == Operator.StructureInstance) {
+                                    operand = readInstance(token);
+                                } else {
+                                    operand = new VariableExpression(value, variables::get, variables::put);
+                                }
+                        }
+                        operands.push(operand);
+                }
+            }
+
+            while (!operators.isEmpty()) {
+                applyTopOperator();
+            }
+
+            return operands.pop();
+        }
+
+        @SneakyThrows
+        private void applyTopOperator() {
+            // e.g. a + b
+            Operator operator = operators.pop();
+            Class<? extends OperatorExpression> operatorType = operator.getType();
+            Expression left = operands.pop();
+            if (BinaryOperatorExpression.class.isAssignableFrom(operatorType)) {
+                Expression right = operands.pop();
+                operands.push(operatorType
+                        .getConstructor(Expression.class, Expression.class)
+                        .newInstance(right, left));
+            } else if (UnaryOperatorExpression.class.isAssignableFrom(operatorType)) {
+                // e.g. new Instance []
+                operands.push(operatorType
+                        .getConstructor(Expression.class)
+                        .newInstance(left));
+            } else {
+                throw new SyntaxException(String.format("Operator `%s` is not supported", operatorType));
+            }
+        }
+
+        private Expression readInstance(Token token) {
+            StructureDefinition definition = structures.get(token.getValue());
+
+            List<Expression> arguments = new ArrayList<>();
+            if (StatementParser.this.peek(TokenType.GroupDivider, "[")) {
+
+                next(TokenType.GroupDivider, "["); //skip open square bracket
+
+                while (!StatementParser.this.peek(TokenType.GroupDivider, "]")) {
+                    Expression value = new ExpressionReader().readExpression();
+                    arguments.add(value);
+
+                    if (StatementParser.this.peek(TokenType.GroupDivider, ","))
+                        next();
+                }
+
+                next(TokenType.GroupDivider, "]"); //skip close square bracket
+            }
+
+            if (definition == null) {
+                throw new SyntaxException(String.format("Structure is not defined: %s", token.getValue()));
+            }
+            return new StructureExpression(definition, arguments, variables::get);
+        }
+
+        private boolean peek(TokenType type, TokenType... types) {
+            TokenType[] tokenTypes = ArrayUtils.add(types, type);
+            if (position < tokens.size()) {
+                Token token = tokens.get(position);
+                return Stream.of(tokenTypes).anyMatch(t -> t == token.getType());
+            }
+            return false;
+        }
     }
 }
