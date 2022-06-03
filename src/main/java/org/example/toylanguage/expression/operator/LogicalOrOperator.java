5bd8a62d4765c844fb63c070ec9c14b78565ff61
Alexander Makeev <alexmakeev1995@gmail.com>
2022-06-03 20:50:43 +0300

    Add LogicalAndOperator and LogicalOrOperator

---
+++ src/main/java/org/example/toylanguage/expression/operator/LogicalOrOperator.java
@@ -0,0 +1,21 @@
+package org.example.toylanguage.expression.operator;
+
+import org.example.toylanguage.exception.ExecutionException;
+import org.example.toylanguage.expression.Expression;
+import org.example.toylanguage.expression.value.LogicalValue;
+import org.example.toylanguage.expression.value.Value;
+
+public class LogicalOrOperator extends BinaryOperatorExpression {
+    public LogicalOrOperator(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public Value<?> calc(Value<?> left, Value<?> right) {
+        if (left instanceof LogicalValue && right instanceof LogicalValue) {
+            return new LogicalValue(((LogicalValue) left).getValue() || ((LogicalValue) right).getValue());
+        } else {
+            throw new ExecutionException(String.format("Unable to perform OR operator for non logical values `%s`, '%s'", left, right));
+        }
+    }
+}
