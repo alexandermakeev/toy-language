3dd9c183283bcad245b42ff4fe46bb1170085cd8
Alexander Makeev <alexmakeev1995@gmail.com>
2023-03-22 00:42:59 +0400

    Add underscore negative lookahead for character lexemes

--- src/main/java/org/example/toylanguage/token/TokenType.java
+++ src/main/java/org/example/toylanguage/token/TokenType.java
@@ -9,14 +9,14 @@ public enum TokenType {
     Comment("\\#.*"),
     LineBreak("[\\n\\r]"),
     Whitespace("[\\s\\t]"),
-    Keyword("(if|elif|else|end|print|input|class|fun|return|loop|in|by|break|next)(?=\\s|$)"),
-    GroupDivider("(\\[|\\]|\\,|\\{|}|[.]{2})"),
-    Logical("(true|false)(?=\\s|$)"),
+    Keyword("(if|elif|else|end|print|input|class|fun|return|loop|in|by|break|next)(?=\\s|$)(?!_)"),
+    GroupDivider("(\\[|\\]|\\,|\\{|}|\\.{2}"),
+    Logical("(true|false)(?=\\s|$)(?!_)"),
     Numeric("([-]?(?=[.]?[0-9])[0-9]*(?![.]{2})[.]?[0-9]*)"),
-    Null("(null)(?=,|\\s|$)"),
-    This("(this)(?=,|\\s|$)"),
+    Null("(null)(?=,|\\s|$)(?!_)"),
+    This("(this)(?=,|\\s|$)(?!_)"),
     Text("\"([^\"]*)\""),
-    Operator("(\\+|-|\\*|/{1,2}|%|>=|>|<=|<{1,2}|={1,2}|!=|!|:{2}\\s+new|:{2}|\\(|\\)|(new|and|or)(?=\\s|$))"),
+    Operator("(\\+|-|\\*|/{1,2}|%|>=|>|<=|<{1,2}|={1,2}|!=|!|:{2}\\s+new|:{2}|\\(|\\)|(new|and|or)(?=\\s|$)(?!_))"),
     Variable("[a-zA-Z_]+[a-zA-Z0-9_]*");
 
     private final String regex;
